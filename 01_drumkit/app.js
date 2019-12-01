e0486217c8cf3dfd265a1848f7730134bb768adb
stefanstifter <stefan.stifter@saswebdev.com>
2019-12-01 20:34:59 +0100

    apply minor style updates

--- 01_drumkit/app.js
+++ 01_drumkit/app.js
@@ -1,17 +1,11 @@
 function playAudio(keyCode) {
-    console.log(keyCode);
-    
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     const button = document.querySelector(`button[data-key="${keyCode}"]`);
-    
     if (!audio) return;
-
     button.classList.add('playing');
-
     audio.currentTime = 0;
     audio.play();
 }
-
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
