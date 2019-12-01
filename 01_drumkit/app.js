87841dfa2cd89607d8fb995919f2cb10c00e0910
stefanstifter <stefan.stifter@saswebdev.com>
2019-12-01 20:01:58 +0100

    style transition and romve it after playing

--- 01_drumkit/app.js
+++ 01_drumkit/app.js
@@ -1,10 +1,20 @@
 function playAudio(keyCode) {
-    let audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const button = document.querySelector(`button[data-key="${keyCode}"]`);
     
     if (!audio) return;
+
+    button.classList.add('playing');
 
     audio.currentTime = 0;
     audio.play();
 }
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
 window.addEventListener('keydown', (e) => playAudio(e.keyCode));
+const keys = Array.from(document.querySelectorAll('.transition'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
